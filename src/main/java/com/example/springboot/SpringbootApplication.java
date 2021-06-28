7a7002820da1765b55ef0e8c1a0e3efee3662b6f
Ramanjaneyulu Dayyala <anjikdk@gmail.com>
2021-06-28 17:43:11 +0530

    Adding OpenAPI -- Swagger UI into the project

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -3,7 +3,15 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		  info = @Info(
+		  title = "Springboot CRUD Example",
+		  description = "In this project we can see all CRUD operations")
+		)
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
